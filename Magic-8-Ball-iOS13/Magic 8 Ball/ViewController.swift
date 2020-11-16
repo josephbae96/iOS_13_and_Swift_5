9d236647dd538562f1e5b6b5e893d0b116b1dbec
Joseph Bae <josephbae@utexas.edu>
2020-11-16 17:56:11 -0600

    New gif

--- Magic-8-Ball-iOS13/Magic 8 Ball/ViewController.swift
+++ Magic-8-Ball-iOS13/Magic 8 Ball/ViewController.swift
@@ -6,16 +6,26 @@
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
-import UIKit
+import UIKit;
 
 class ViewController: UIViewController {
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")];
 
-    @IBOutlet weak var imageView: UIImageView!
-            
+    @IBOutlet weak var imageView: UIImageView!;
+    @IBOutlet weak var askMeAnythingText: UILabel!;
+    
     @IBAction func askButtonPressed(_ sender: Any) {
-        imageView.image = ballArray.randomElement()
+        imageView.image = ballArray.randomElement();
+        if (imageView.image == ballArray[1]){
+            imageView.image = #imageLiteral(resourceName: "hal");
+            askMeAnythingText.minimumScaleFactor = 0.5;
+            askMeAnythingText.adjustsFontSizeToFitWidth = true;
+            askMeAnythingText.text = "I'm sorry, Dave,I'm afraid I can't do that";
+        } else {
+            askMeAnythingText.text = "Ask me anything...";
+        }
+        
     }
 }
 
