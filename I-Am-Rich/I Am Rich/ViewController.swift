4e6b708d8c983c91125241e35cb272d309e2c46d
Joseph Bae <josephbae@utexas.edu>
2020-11-16 17:23:28 -0600

    Initial commit

---
+++ I-Am-Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Joseph Bae on 11/13/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# iOS_13_and_Swift_5
+ The Complete iOS App Development Bootcamp taught by Dr. Angela Yu on Udemy  The iOS projects in this repo have been created by Joseph Bae as he follows along the Udemy course.
