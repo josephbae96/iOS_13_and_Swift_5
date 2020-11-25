618d4a260597ac8074d033235358e6f60c7b10cc
Joseph Bae <josephbae@utexas.edu>
2020-11-25 16:10:56 -0600

    Tipsy Initial Commit

---
+++ Tipsy/Tipsy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Joseph Bae on 11/25/2020.
+//  Copyright © 2020 By Bae. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
