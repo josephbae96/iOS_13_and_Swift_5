62d220bd563e84bf26349e83b9a8efff6f944e3c
Joseph Bae <josephbae@utexas.edu>
2020-11-25 16:46:22 -0600

    Tipsy Completion

---
+++ Tipsy/Tipsy/ResultsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by Joseph Bae on 11/25/20.
+//  Copyright © 2020 Joseph Bae. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    var result = "0.0"
+    var tip = 10
+    var split = 2
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        totalLabel.text = result
+        settingsLabel.text = "Split between \(split) people, with \(tip)% tip."
+    }
+    
+
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
