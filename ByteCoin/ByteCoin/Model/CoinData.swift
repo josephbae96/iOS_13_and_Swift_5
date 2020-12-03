d728da474f536bfb275470eb055185d5af916961
Joseph Bae <josephbae@utexas.edu>
2020-12-03 12:46:04 -0600

    ByteCoin Complete

---
+++ ByteCoin/ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Joseph Bae on 12/2/20.
+//  Copyright © 2020 Joseph Bae. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Codable {
+    let rate: Double
+}
