c874b7aa54a3056fc62bdc27cccdbf960c0182db
Joseph Bae <josephbae@utexas.edu>
2021-01-08 18:09:07 -0600

    BaeCard Business Card Application Complete

---
+++ BaeCard/BaeCard/ContentView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  BaeCard
+//
+//  Created by Joseph Bae on 1/8/21.
+//  Created in accordance to Angela Yu
+//  as part of the iOS and Swift Complete Bootcamp
+//  from Udemy. This project was created entirely
+//  in SwiftUI.
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack{
+            Color(red: 0.09, green: 0.63, blue: 0.52)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("bae")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150, height: 150)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .overlay(
+                        Circle().stroke(Color.white, lineWidth: 5)
+                    )
+                Text("Joseph Bae")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                    .padding()
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                Divider()
+                InfoView(text: "(361) 523-3056", imageName: "phone.fill")
+                InfoView(text: "josephbae96@gmail.com",
+                    imageName: "envelope.fill")
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
---
+++ BaeCard/BaeCard/InfoView.swift
@@ -0,0 +1,35 @@
+//
+//  InfoView.swift
+//  BaeCard
+//
+//  Created by Joseph Bae on 1/8/21.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .fill(Color.white)
+            .frame(height: 50)
+            .foregroundColor(.white)
+            .overlay(HStack {
+                Image(systemName: imageName)
+                    .foregroundColor(.green)
+                Text(text)
+                    .foregroundColor(Color("Info Color"))
+            })
+            .padding(.all)
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView(text: "hello", imageName: "phone.fill")
+            .previewLayout(.sizeThatFits)
+    }
+}
